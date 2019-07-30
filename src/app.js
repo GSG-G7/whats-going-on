18e745c6ff87275c22636b5dc93a0eb35392d741
FadiAlamassi <alamassifadi@gmail.com>
2019-07-30 15:27:30 +0300

    initialize express app, handle home page and statics, set the listining port Relates #7

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+app.set('port', process.env.PORT || 5040);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+module.exports = app;
