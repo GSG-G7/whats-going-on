7205a51ec8a0edc302b553a2b709f3ba61fe77bc
alaa-yasin <alaayasin.ps@gmail.com>
2019-07-31 11:10:03 +0300

    add all test files inside this folder Relates #23

---
+++ tests/server_tests/searchpage-test.js
@@ -0,0 +1,18 @@
+const test = require('tape');
+const supertest = require('supertest');
+const app = require('../../src/app');
+
+const searchPageTest = test('search route returns status code 200', (t) => {
+  supertest(app)
+    .get('/search/amd')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, 'status code should be 200');
+      t.isNotEqual(res.body.articles[0].title.toLowerCase().indexOf('amd'), -1, 'the title should at least have the search query');
+      t.end();
+    });
+});
+
+module.exports = searchPageTest;
