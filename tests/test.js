068f0aa3c5c166f48bf9cb974c670046942ea5e5
alaa-yasin <alaayasin.ps@gmail.com>
2019-07-31 11:08:02 +0300

    separate tests into files Relates #23

--- tests/test.js
+++ tests/test.js
@@ -1,32 +1,12 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../src/app');
+const homePageTest = require('./server tests/homepage-test');
+const latestPageTest = require('./server tests/latestpage-test');
+const searchPageTest = require('./server tests/searchpage-test');
 
 
 test('Testing is working', (t) => {
   t.equal(1, 1, 'One should equal one');
   t.end();
-});
-test('Home root return status code 200', (t) => {
-  supertest(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', /html/)
-    .end((err, res) => {
-      t.error(err);
-      t.equal(res.statusCode, 200, 'Status Code should be 200');
-      t.end();
-    });
-});
-
-test('latest news root return status code 200', (t) => {
-  supertest(app)
-    .get('/latest')
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .end((err, res) => {
-      t.error(err);
-      t.equal(res.statusCode, 200, 'Status Code should be 200');
-      t.end();
-    });
 });
