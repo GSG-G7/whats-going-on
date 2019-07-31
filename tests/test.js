db89527ff102ed223033f91adf9deac494cb2e7c
alaa-yasin <alaayasin.ps@gmail.com>
2019-07-31 11:20:28 +0300

    some edits Relates #23

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,10 @@
 const test = require('tape');
-const supertest = require('supertest');
-const app = require('../src/app');
-const homePageTest = require('./server tests/homepage-test');
-const latestPageTest = require('./server tests/latestpage-test');
-const searchPageTest = require('./server tests/searchpage-test');
-
 
 test('Testing is working', (t) => {
   t.equal(1, 1, 'One should equal one');
   t.end();
 });
+
+require('./server_tests/homepage-test');
+require('./server_tests/latestpage-test');
+require('./server_tests/searchpage-test');
