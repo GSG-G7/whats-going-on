7411c471a14b1bf3d1cf094026d30464e6ecb85b
alaa-yasin <alaayasin.ps@gmail.com>
2019-07-31 14:19:15 +0300

    add http://localhost: to the listen function Relates #27

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is listning on ${app.get('port')}`);
+  console.log(`Server is listning on port http://localhost:${app.get('port')}`);
 });
