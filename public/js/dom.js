025c823559369caf6d203b15b367f718df6ac5f3
Amoodaa <amoodaa@gmail.com>
2019-07-31 13:18:24 +0300

    added style for details section and anchors relates #14

--- public/js/dom.js
+++ public/js/dom.js
@@ -28,14 +28,19 @@ const renderArticles = (details) => {
     articleLink.textContent = e.title;
     header.appendChild(articleLink);
 
+    const dateDiv = create('div');
     const publishTime = create('h4');
-    publishTime.textContent = `published ${getHoursAgo(e.publishedAt)} hours ago`;
-    contentDiv.appendChild(publishTime);
+    const clockIcon = create('i');
+    clockIcon.className = 'far fa-clock';
+    publishTime.textContent = `${getHoursAgo(e.publishedAt)} hours ago`;
+    dateDiv.appendChild(clockIcon);
+    dateDiv.appendChild(publishTime);
+    contentDiv.appendChild(dateDiv);
     const newsContent = create('p');
     newsContent.textContent = e.description;
     contentDiv.appendChild(newsContent);
     contentDiv.className = 'news__details';
-
+    
     div.appendChild(imageDiv);
     div.appendChild(contentDiv);
     div.className = 'news__article';
