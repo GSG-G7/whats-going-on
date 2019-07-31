acc8eef6a0a7c3906477c943fe49bbb9459ee838
alaa-yasin <alaayasin.ps@gmail.com>
2019-07-31 11:29:59 +0300

    forgot to save solving the conflict

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=12b90d1e4e264edf8eedf0be2f0992a2';
-// eslint-disable-next-line no-console
-console.log(url);
-=======
->>>>>>> a276a73c7644831699ecd4cabc82d36fc7718169
 const selector = id => document.querySelector(`#${id}`);
 const create = tag => document.createElement(`${tag}`);
 const userInput = selector('form__userinput');
@@ -46,14 +40,6 @@ const renderArticles = (details) => {
   newContainer.className = 'container';
   selector('news-section').replaceChild(newContainer, selector('container'));
 };
-<<<<<<< HEAD
-userInput.addEventListener('keyup', (e) => {
-  // eslint-disable-next-line no-unused-vars
-  const input = e.target.value;
-});
-=======
-
->>>>>>> a276a73c7644831699ecd4cabc82d36fc7718169
 
 searchBtn.addEventListener('click', () => {
   const input = userInput.value;
